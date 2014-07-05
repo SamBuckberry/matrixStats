8b9b4f713b6803f3dfd1084f383d8144646c7dd5
SamBuckberry <sambuckberry@gmail.com>
2014-07-05 14:57:19 +0930

    Added rf stats function

---
+++ calc_rf_stats_function.R
@@ -0,0 +1,47 @@
+library(tools)
+
+workingDirectory <- "~/Desktop/"
+filePattern="*.rf"
+
+calc_rf_stats <- function(workingDirectory, filePattern="*.rf"){
+        
+        # Get the matrix files for calculating stats
+        filenames <- list.files(path=workingDirectory, pattern=filePattern)
+        
+        # Function for calculating matrix stats
+        stats <- function(filename) {
+                        ## Load data matrices from HashRF analysis
+                        file <- filename
+                        T <- as.matrix(read.table(file))
+                        
+                        # Discard diagonal and lower triangle of symmetrical matrix
+                        T[lower.tri(T, diag=TRUE)] <- NA
+                        #print(T) ## just to check has done the replacement by NA
+                        
+                        # Return the statistics for the matrix
+                        return(c(mean(T, na.rm=TRUE), sd(T, na.rm=TRUE), 
+                          median(T, na.rm=TRUE),max(T, na.rm=TRUE),
+                          min(T, na.rm=TRUE)))
+                }
+
+        # apply the stats function to the list of files
+        result <- lapply(X=filenames, FUN=stats)
+        # convert results list into data.frame
+        result <- do.call(rbind.data.frame, result, quote=FALSE)
+        colnames(result) <- c("Mean", "StdDev", "Median", "Max", "Min")
+        
+        # Add the names for each file as row.names for results
+        row.names(result) <- lapply(X=filenames,
+                                    FUN=function(x){file_path_sans_ext(x)})
+        
+        # Calculate normalised mean and add to results
+        tot <- colSums(result)
+        GENE_tot <- as.matrix(result[,1]/tot[1])
+        colnames(GENE_tot) <- c("Norm_Mean")
+        Total <- cbind(result, GENE_tot)
+        
+        # Return the data frame of results
+        return(Total)
+}
+
+calc_rf_stats(workingDirectory, filePattern)
